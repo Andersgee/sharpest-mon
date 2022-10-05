09621dab76fa9cda2b8d5b7e5ed2e94742cc22ce
Anders Gustafsson <andersgee@gmail.com>
2022-10-05 04:05:59 +0200

    tidy script

--- scripts/create_pokemon.ts
+++ scripts/create_pokemon.ts
@@ -2,16 +2,15 @@ import "dotenv/config";
 import { prisma } from "../src/server/db/client";
 import { ALL_MONS } from "../src/utils/mons";
 
-/*
-1. add 
-"type": "module"
- in package.json
-
- 2. 
- yarn tsnode ./scripts/create_dummydata.ts
-*/
-
+/**
+ * must have "type": "module" in package.json
+ * also see package.json script tsnode
+ * ```sh
+ * yarn tsnode ./scripts/create_pokemon.ts
+ * ```
+ */
 async function main() {
+  /*
   for (let i = 0; i < ALL_MONS.length; i++) {
     const res = await prisma.pokemon.create({
       data: {
@@ -20,6 +19,10 @@ async function main() {
     });
     console.log(res);
   }
+  */
+  const pokemondata = ALL_MONS.map((_, i) => ({ id: i }));
+  const res = await prisma.pokemon.createMany({ data: pokemondata });
+  console.log(res);
 }
 
 main();
